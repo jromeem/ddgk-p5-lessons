73dc9b998549f89cd1cf8489b96155cf663c7dd7
Jerome Martinez <jerome.e.martinez@gmail.com>
2017-02-04 11:56:10 -0600

    variables, conditionals, loops, pracitice

---
+++ 002/loops-practice/loops-practice.js
@@ -0,0 +1,10 @@
+console.log('hello loops-practice!');
+function setup() {
+    var canvas = createCanvas(500, 500);
+    canvas.parent("p5canvas");
+}
+
+function draw() {
+    background(100);
+    ellipse(200, 200, 100, 100);
+}
