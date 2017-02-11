88a9557a420d4d05a222bd8c7531627c933ba1cf
Jerome Martinez <jerome.e.martinez@gmail.com>
2017-02-11 12:34:08 -0600

    function exercise

--- 003/functions_exercise/functions_exercise.js
+++ 003/functions_exercise/functions_exercise.js
@@ -1,10 +1,42 @@
 console.log('hello functions_exercise!');
+
+// global variables
+var randomX;
+var randomY;
+var squareSize;
+
+var cornerX;
+var cornerY;
+
 function setup() {
-    var canvas = createCanvas(500, 500);
-    canvas.parent("p5canvas");
+  var canvas = createCanvas(windowWidth, windowHeight);
+  canvas.parent("p5canvas");
+
+  // make random values
+  randomX = random(0, width);
+  randomY = random(0, height);
+
+  rectMode(CORNERS);
 }
 
 function draw() {
-    background(100);
-    ellipse(200, 200, 100, 100);
+  background(100);
+
+  // find the corners
+  findCorners();
+  rect(randomX, randomY, cornerX, cornerY);
+}
+
+function findCorners() {
+  //** write your code here **//
+  
+  cornerX = mouseX;
+  cornerY = mouseY;
+
+  // make a variable with the absolute distance between the mouse and the randomX
+  // make a variable with absolute distance between the mouse and the randomY
+  // make a variable that contains the minimum distance between the two previous distances
+  // calculate the opposite corners: cornerX and cornerY
+
+  //** end of your code **//
 }
