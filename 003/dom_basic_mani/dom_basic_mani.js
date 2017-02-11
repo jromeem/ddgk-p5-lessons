7a7ca73361c9a7e5d715bbdc9f9e0bc860a090b6
Jerome Martinez <jerome.e.martinez@gmail.com>
2017-02-11 13:09:24 -0600

    basic dom manipulation

--- 003/dom_basic_mani/dom_basic_mani.js
+++ 003/dom_basic_mani/dom_basic_mani.js
@@ -1,10 +1,42 @@
-console.log('hello dom_basic_mani!');
+var dancingWords = [];
+
+function DanceSpan(element, x, y) {
+  element.position(x, y);
+  
+  this.brownian = function() {
+    x += random(-6, 6);
+    y += random(-6, 6);
+    element.position(x, y);
+  };
+};
+
 function setup() {
-    var canvas = createCanvas(500, 500);
-    canvas.parent("p5canvas");
+  // This paragraph is created aside of the main block of code.
+  // It's to differentiate the creation of an element from its 
+  // selection. Selected elements doesn't need to be created by 
+  // p5js, they can be just plain HTML.
+  // createP('I learne in this Letter, that Don Peter of Arragon, '
+  //   + ' comes this night to Messina').addClass('text');
+
+  // This line grabs the paragraph just created, but it would 
+  // also grab any other elements with class 'text' in the HTML
+  // page.
+  var texts = selectAll('.text');
+
+  for (var i=0; i<texts.length; i++) {
+    var paragraph = texts[i].html();
+    var words = paragraph.split(' ');
+    for (var j=0; j<words.length; j++) {   
+      var spannedWord = createSpan(words[j]);
+      var dw = new DanceSpan(spannedWord, random(windowWidth), random(windowHeight));
+      dancingWords.push(dw);
+    };   
+  };
 }
 
 function draw() {
-    background(100);
-    ellipse(200, 200, 100, 100);
+  for (var i=0; i<dancingWords.length; i++) {
+    dancingWords[i].brownian();
+  };
 }
+
