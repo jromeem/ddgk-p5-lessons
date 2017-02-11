4d9345c91e822e015ce9f5bf447ff2a3d8a1c25a
Jerome Martinez <jerome.e.martinez@gmail.com>
2017-02-11 10:54:26 -0600

    flying obama

--- 003/media_images/media_images.js
+++ 003/media_images/media_images.js
@@ -1,10 +1,56 @@
 console.log('hello media_images!');
-function setup() {
-    var canvas = createCanvas(500, 500);
-    canvas.parent("p5canvas");
+
+// image variables
+var wings;
+var obama;
+
+var flyingObama_x = 500/2; // x position
+var flyingObama_y = 500/2; // y position
+var flyingObama_w = 200;   // width
+var flyingObama_h = 200;   // height
+var flyingObama_s = 2;     // speed
+
+var wingsWidth = flyingObama_w;
+var wingsflapSpeed = 20;
+var wingXOffsset = 0;
+var wingYOffset = 50;
+
+function preload() {
+  wings = loadImage("assets/wings.png");
+  obama = loadImage("assets/obama.png");
 }
 
-function draw() {
-    background(100);
-    ellipse(200, 200, 100, 100);
+function setup() {
+  var canvas = createCanvas(500, 500);
+  canvas.parent("p5canvas");
+  imageMode(CENTER);
+}
+
+function draw() { 
+  background(255);
+
+  flapWings();
+  flyAround();
+  displayFlyingObama();
+  
+}
+
+function flapWings() {
+  wingsWidth += wingsflapSpeed;
+  if (wingsWidth < 50 || wingsWidth > flyingObama_w) {
+    wingsflapSpeed = -wingsflapSpeed;
+  }
+}
+
+function flyAround() {
+  flyingObama_x = flyingObama_x + flyingObama_s;
+  flyingObama_y = flyingObama_y + flyingObama_s;
+  if (flyingObama_x > width || flyingObama_x < 0) {
+    flyingObama_s = -flyingObama_s;
+  }
+}
+
+function displayFlyingObama() {
+  image(wings, flyingObama_x, flyingObama_y-wingYOffset, wingsWidth, flyingObama_h*2/3);
+  image(obama, flyingObama_x, flyingObama_y, flyingObama_w, flyingObama_h);
 }
